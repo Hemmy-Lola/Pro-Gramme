49cd0ffd2dd0ec060f50db1d34a20440fca3e94f
hemmy-lola <hl.mathys@gmail.com>
2025-05-22 00:10:02 +0200

    feat: add  navbar + responsive effect

---
+++ js/nav.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navMenu = document.querySelector('.nav-menu');
+
+    burgerMenu.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        burgerMenu.classList.toggle('active');
+    });
+
+    document.addEventListener('click', (e) => {
+        if (!navMenu.contains(e.target) && !burgerMenu.contains(e.target)) {
+            navMenu.classList.remove('active');
+            burgerMenu.classList.remove('active');
+        }
+    });
+});
